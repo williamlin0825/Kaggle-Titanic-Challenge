ec4621d7354dca0f61db98f9c79ea88fecd3d01b
William Lin <willlin890825@gmail.com>
2021-06-01 15:45:42 +0800

    Add files via upload

--- logistic_regression.py
+++ logistic_regression.py
@@ -26,9 +26,11 @@ test = pd.read_csv("test.csv")
 # ============================================================================
 # Change Column "Name" to "Full Name"
 train = train.rename(columns = {"Name" : "Full Name"})
+test = test.rename(columns = {"Name" : "Full Name"})
 
 # Add "Family = SibSp + Parch Column"
-train.insert(8, "Family", train["SibSp"] + train["Parch"] + 1) # 在船上的家族人數(有包含自己)
+train.insert(8, "Family", train["SibSp"] + train["Parch"] + 1)
+test.insert(7, "Family", test["SibSp"] + test["Parch"] + 1)
 
 # %%% Fare
 # 在還沒填補Pclass值之前先算各Pclass的Fare平均，避免因Pclass填補誤差造成Fare平均的誤差
@@ -38,13 +40,39 @@ Fare_Pclass3_mean = train.dropna(axis = 0, how = "all", subset = ["Fare"]).dropn
 
 train["Fare"].fillna(-1, inplace = True)
 for i in range(len(train)):
-    if train["Fare"][i] == -1:
+    if train["Fare"][i] == -1: # Refer to Ticket
+        train_drop_nan = train.drop(train[train["Fare"] == -1].index)
+        if train_drop_nan[train_drop_nan["Ticket"] == train["Ticket"].iloc[i]].shape[0] != 0:
+            # Find Mode
+            mode_counts = np.bincount(train_drop_nan[train_drop_nan["Ticket"] == train["Ticket"].iloc[i]]["Fare"])
+            mode = np.argmax(mode_counts)
+            # Fill Mode
+            train["Fare"][i] = mode
+    if train["Fare"][i] == -1: # Fill Each Class Fare Mean
         if train["Pclass"][i] == 1:
             train["Fare"][i] = Fare_Pclass1_mean
         if train["Pclass"][i] == 2:
             train["Fare"][i] = Fare_Pclass2_mean
         if train["Pclass"][i] == 3:
             train["Fare"][i] = Fare_Pclass3_mean
+            
+test["Fare"].fillna(-1, inplace = True)
+for i in range(len(test)):
+    if test["Fare"][i] == -1: # Refer to Ticket
+        train_drop_nan = train.drop(train[train["Fare"] == -1].index)
+        if train_drop_nan[train_drop_nan["Ticket"] == test["Ticket"].iloc[i]].shape[0] != 0:
+            # Find Mode
+            mode_counts = np.bincount(train_drop_nan[train_drop_nan["Ticket"] == test["Ticket"].iloc[i]]["Fare"])
+            mode = np.argmax(mode_counts)
+            # Fill Mode
+            test["Fare"][i] = mode
+    if test["Fare"][i] == -1:
+        if test["Pclass"][i] == 1:
+            test["Fare"][i] = Fare_Pclass1_mean
+        if test["Pclass"][i] == 2:
+            test["Fare"][i] = Fare_Pclass2_mean
+        if test["Pclass"][i] == 3:
+            test["Fare"][i] = Fare_Pclass3_mean
 
 # %%% Pclass
 # Fill Pclass Method 1
@@ -91,6 +119,7 @@ for i in range(len(test)):
 """
 
 # Fill Pclass Method 2
+"""
 # Age Distribution of Pclass
 plt.bar(train[train["Pclass"] == 1]["Age"].value_counts().index, train[train["Pclass"] == 1]["Age"].value_counts())
 plt.title("Age Distribution of Pclass 1")
@@ -113,27 +142,37 @@ plt.ylabel("Number of people")
 plt.xlim([0, 100])
 plt.ylim([0, 25])
 plt.show()
+"""
 
 # Max Fare of Pclass 2 & 3
 max_fare_pclass_2and3 = train[train["Pclass"] > 1].max(skipna = True)["Fare"]
 
 # Replace Nan in Cabin Row With -1
 train["Pclass"].fillna(-1, inplace = True)
+test["Pclass"].fillna(-1, inplace = True)
 
 # Fill Pclass
 for i in range(len(train)):
+    if train["Pclass"][i] == -1: # Refer to Ticket
+        train_drop_nan = train.drop(train[train["Pclass"] == -1].index)
+        if train_drop_nan[train_drop_nan["Ticket"] == train["Ticket"].iloc[i]].shape[0] != 0:
+            # Find Mode
+            mode_counts = np.bincount(train_drop_nan[train_drop_nan["Ticket"] == train["Ticket"].iloc[i]]["Pclass"])
+            mode = np.argmax(mode_counts)
+            # Fill Mode
+            train["Pclass"][i] = mode
     if train["Pclass"][i] == -1: # Refer to Cabin
         if "T" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
-        if "A" in str(train["Cabin"][i]):
+        elif "A" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
-        if "B" in str(train["Cabin"][i]):
+        elif "B" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
-        if "C" in str(train["Cabin"][i]):
+        elif "C" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
-        if "D" in str(train["Cabin"][i]):
+        elif "D" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
-        if "E" in str(train["Cabin"][i]):
+        elif "E" in str(train["Cabin"][i]):
             train["Pclass"][i] = 1
     if train["Pclass"][i] == -1: # Refer to Fare
         if train["Pclass"][i] > max_fare_pclass_2and3:
@@ -141,12 +180,46 @@ for i in range(len(train)):
     if train["Pclass"][i] == -1: # Refer to Embarked
         if train["Embarked"][i] == "Q":
             train["Pclass"][i] = 3
+            
+for i in range(len(test)):
+    if test["Pclass"][i] == -1: # Refer to Ticket
+        train_drop_nan = train.drop(train[train["Pclass"] == -1].index)
+        if train_drop_nan[train_drop_nan["Ticket"] == test["Ticket"].iloc[i]].shape[0] != 0:
+            # Find Mode
+            mode_counts = np.bincount(train_drop_nan[train_drop_nan["Ticket"] == test["Ticket"].iloc[i]]["Pclass"])
+            mode = np.argmax(mode_counts)
+            # Fill Mode
+            test["Pclass"][i] = mode
+    if test["Pclass"][i] == -1: # Refer to Cabin
+        if "T" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+        elif "A" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+        elif "B" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+        elif "C" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+        elif "D" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+        elif "E" in str(test["Cabin"][i]):
+            test["Pclass"][i] = 1
+    if test["Pclass"][i] == -1: # Refer to Fare
+        if test["Pclass"][i] > max_fare_pclass_2and3:
+            test["Pclass"][i] = 1
+    if test["Pclass"][i] == -1: # Refer to Embarked
+        if test["Embarked"][i] == "Q":
+            test["Pclass"][i] = 3
 
 # %%% Name
 # Split Last Name, Title and First Name
 train.insert(4, "Last Name", train["Full Name"].str.split(", ", expand = True).iloc[:, 0])
 train.insert(5, "Title", train["Full Name"].str.split(", ", expand = True).iloc[:, 1].str.split(".", expand = True).iloc[:, 0])
 train.insert(6, "First Name", train["Full Name"].str.split(", ", expand = True).iloc[:, 1].str.split(".", expand = True).iloc[:, 1].map(lambda x: str(x)[1:]))
+
+test.insert(3, "Last Name", test["Full Name"].str.split(", ", expand = True).iloc[:, 0])
+test.insert(4, "Title", test["Full Name"].str.split(", ", expand = True).iloc[:, 1].str.split(".", expand = True).iloc[:, 0])
+test.insert(5, "First Name", test["Full Name"].str.split(", ", expand = True).iloc[:, 1].str.split(".", expand = True).iloc[:, 1].map(lambda x: str(x)[1:]))
+
 """
 print(train["Title"].value_counts()) # number of people of each title
 """
@@ -182,71 +255,171 @@ for i in range(len(train)):
     if train["Age"][i] == -1:
         if train["Title"][i] == "Mr":
             train["Age"][i] = Age_Mr_mean
-        if train["Title"][i] == "Miss":
+        elif train["Title"][i] == "Miss":
             train["Age"][i] = Age_Miss_mean
-        if train["Title"][i] == "Mrs":
+        elif train["Title"][i] == "Mrs":
             train["Age"][i] = Age_Mrs_mean
-        if train["Title"][i] == "Master":
+        elif train["Title"][i] == "Master":
             train["Age"][i] = Age_Master_mean
-        if train["Title"][i] == "Dr":
+        elif train["Title"][i] == "Dr":
             train["Age"][i] = Age_Dr_mean
-        if train["Title"][i] == "Rev":
+        elif train["Title"][i] == "Rev":
             train["Age"][i] = Age_Rev_mean
-        if train["Title"][i] == "Col":
+        elif train["Title"][i] == "Col":
             train["Age"][i] = Age_Col_mean
-        if train["Title"][i] == "Major":
+        elif train["Title"][i] == "Major":
             train["Age"][i] = Age_Major_mean
-        if train["Title"][i] == "Jonkheer":
+        elif train["Title"][i] == "Jonkheer":
             train["Age"][i] = Age_Jonkheer_mean
-        if train["Title"][i] == "Mlle":
+        elif train["Title"][i] == "Mlle":
             train["Age"][i] = Age_Mlle_mean
-        if train["Title"][i] == "Mme":
+        elif train["Title"][i] == "Mme":
             train["Age"][i] = Age_Mme_mean
-        if train["Title"][i] == "Ms":
+        elif train["Title"][i] == "Ms":
             train["Age"][i] = Age_Miss_mean
-        if train["Title"][i] == "Capt":
+        elif train["Title"][i] == "Capt":
             train["Age"][i] = Age_Capt_mean
-        if train["Title"][i] == "Sir":
+        elif train["Title"][i] == "Sir":
             train["Age"][i] = Age_Sir_mean
         else:
             train["Age"][i] = Age_total_mean
 
+test["Age"].fillna(-1, inplace = True)
+for i in range(len(test)):
+    if test["Age"][i] == -1:
+        if test["Title"][i] == "Mr":
+            test["Age"][i] = Age_Mr_mean
+        elif test["Title"][i] == "Miss":
+            test["Age"][i] = Age_Miss_mean
+        elif test["Title"][i] == "Mrs":
+            test["Age"][i] = Age_Mrs_mean
+        elif test["Title"][i] == "Master":
+            test["Age"][i] = Age_Master_mean
+        elif test["Title"][i] == "Dr":
+            test["Age"][i] = Age_Dr_mean
+        elif test["Title"][i] == "Rev":
+            test["Age"][i] = Age_Rev_mean
+        elif test["Title"][i] == "Col":
+            test["Age"][i] = Age_Col_mean
+        elif test["Title"][i] == "Major":
+            test["Age"][i] = Age_Major_mean
+        elif test["Title"][i] == "Jonkheer":
+            test["Age"][i] = Age_Jonkheer_mean
+        elif test["Title"][i] == "Mlle":
+            test["Age"][i] = Age_Mlle_mean
+        elif test["Title"][i] == "Mme":
+            test["Age"][i] = Age_Mme_mean
+        elif test["Title"][i] == "Ms":
+            test["Age"][i] = Age_Miss_mean
+        elif test["Title"][i] == "Capt":
+            test["Age"][i] = Age_Capt_mean
+        elif test["Title"][i] == "Sir":
+            test["Age"][i] = Age_Sir_mean
+        else:
+            test["Age"][i] = Age_total_mean
+
 # %%% Embarked
-"""
-test["Embarked"].fillna(-1, inplace=True)
-for i in range(len(train)):
+test["Embarked"].fillna(-1, inplace = True)
+for i in range(len(test)):
     if test["Embarked"][i] == -1:
         test["Embarked"][i] = "S"
+
+# %%% Age Group
+# Age Distribution Plot
 """
+plt.bar(train["Age"].value_counts().index, train["Age"].value_counts())
+plt.title("Age Distribution")
+plt.xlabel("Age")
+plt.ylabel("Number of people")
+plt.show()
+"""
+
+train["Age Group"] = 0
+for i in range(len(train)): # ~12 : 1 ; 12~60 : 2 ; 60~ : 3
+    if train["Age"].iloc[i] < 12:
+        train["Age Group"].iloc[i] = 1
+    elif train["Age"].iloc[i] >= 12 and train["Age"].iloc[i] < 60:
+        train["Age Group"].iloc[i] = 2
+    elif train["Age"].iloc[i] >= 60:
+        train["Age Group"].iloc[i] = 3
+        
+test["Age Group"] = 0
+for i in range(len(test)): # ~12 : 1 ; 12~60 : 2 ; 60~ : 3
+    if test["Age"].iloc[i] < 12:
+        test["Age Group"].iloc[i] = 1
+    elif test["Age"].iloc[i] >= 12 and test["Age"].iloc[i] < 60:
+        test["Age Group"].iloc[i] = 2
+    elif test["Age"].iloc[i] >= 60:
+        test["Age Group"].iloc[i] = 3
+
+# %%% Relationship
+# Number of People Who Share One Ticket Number Distribution
+plt.bar(train["Ticket"].value_counts().value_counts().index, train["Ticket"].value_counts().value_counts())
+plt.title("Number of People Who Share One Ticket Number Distribution")
+plt.xlabel("Number of People Who Share One Ticket Number")
+plt.ylabel("Number")
+plt.show()
+
+# 先用船票判斷是否一起再用年齡整理關係
+
+# Family = 2
+# Husband(211) and Wife(212)
+# Meaning of the number (eg. 211) : "2" people, case "1", no."1"
+
+# Dad and Child
+
+# Mom and Child
+
+
+# Family = 3
+# Dad and Mom and Child
 
 # %% To Do List
 # ============================================================================
-# Test也要填值
 # 把所有填值都合併在一個for迴圈內
+
+
 def set_Cabin_type(df):
     df.loc[ (df.Cabin.notnull()), 'Cabin' ] = "Yes"
     df.loc[ (df.Cabin.isnull()), 'Cabin' ] = "No"
     return df
 
 train=set_Cabin_type(train)
+test=set_Cabin_type(test)
 
+train_temp=train
+print(train_temp)
+
+#因子化
 d_Sex=pd.get_dummies(train['Sex'],prefix='Sex')
 d_Pclass=pd.get_dummies(train['Pclass'],prefix='Pclass')
 d_Embarked=pd.get_dummies(train['Embarked'],prefix='Embarked')
 d_Cabin=pd.get_dummies(train['Cabin'],prefix='Cabin')
-train=pd.concat([train,d_Sex,d_Pclass],axis=1)
-train.drop(['Full Name','Last Name','Title','First Name','Sex','Pclass','Embarked','Cabin','Ticket','Family','Embarked','Cabin'],axis=1,inplace=True)
-print(train)
+train=pd.concat([train,d_Sex,d_Pclass,d_Embarked,d_Cabin],axis=1)
+train.drop(['Full Name','Last Name','Title','First Name','Sex','Pclass','Embarked','Cabin','Ticket'],axis=1,inplace=True)
 
-#Normalize Age and Fare
+d_Sex1=pd.get_dummies(test['Sex'],prefix='Sex')
+d_Pclass1=pd.get_dummies(test['Pclass'],prefix='Pclass')
+d_Embarked1=pd.get_dummies(test['Embarked'],prefix='Embarked')
+d_Cabin1=pd.get_dummies(test['Cabin'],prefix='Cabin')
+test=pd.concat([test,d_Sex1,d_Pclass1,d_Embarked1,d_Cabin1],axis=1)
+test.drop(['Full Name','Last Name','Title','First Name','Sex','Pclass','Embarked','Cabin','Ticket'],axis=1,inplace=True)
+
+
+#standardize Age and Fare
+
 train_normalize_max_age=((train['Age']-train['Age'].mean())/train['Age'].std()).max()
+train_normalize_min_age=((train['Age']-train['Age'].mean())/train['Age'].std()).min()
+test['Age'] = 2*(((test['Age']-train['Age'].mean())/train['Age'].std())-train_normalize_min_age)/(train_normalize_max_age-train_normalize_min_age)-1
+train['Age'] = 2*(((train['Age']-train['Age'].mean())/train['Age'].std())-train_normalize_min_age)/(train_normalize_max_age-train_normalize_min_age)-1
+
+
 train_normalize_max_fare=((train['Fare']-train['Fare'].mean())/train['Fare'].std()).max()
+train_normalize_min_fare=((train['Fare']-train['Fare'].mean())/train['Fare'].std()).min()
+test['Fare'] = 2*(((test['Fare']-train['Fare'].mean())/train['Fare'].std())-train_normalize_min_fare)/(train_normalize_max_fare-train_normalize_min_fare)-1
+train['Fare'] = 2*(((train['Fare']-train['Fare'].mean())/train['Fare'].std())-train_normalize_min_fare)/(train_normalize_max_fare-train_normalize_min_fare)-1
 
-test['Age'] = ((test['Age']-train['Age'].mean())/train['Age'].std())/train_normalize_max_age
-test['Fare'] = ((test['Fare']-train['Fare'].mean())/train['Fare'].std())/train_normalize_max_fare
 
-train['Age'] = ((train['Age']-train['Age'].mean())/train['Age'].std())/train_normalize_max_age
-train['Fare'] = ((train['Fare']-train['Fare'].mean())/train['Fare'].std())/train_normalize_max_fare
 
 
 #Split Training set value to train_of_train and train_of_test
@@ -264,7 +437,6 @@ train.drop(['PassengerId','Survived'], axis=1,inplace=True)             #drop th
 passenger=test.loc[:,['PassengerId']]
 test.drop(['PassengerId'], axis=1,inplace=True)
 
-print(train)
 #change to array to compute
 y_train_of_train=y_train_of_train.to_numpy()
 y_test_of_train=y_test_of_train.to_numpy()
@@ -290,6 +462,8 @@ def sigmoid(input):
     output = 1 / (1 + np.exp(-input))
     return output
 
+weight_history=[]
+bias_history=[]
 
 def optimize(x, y,learning_rate,iterations,parameters): 
     size = x.shape[0]
@@ -301,7 +475,9 @@ def optimize(x, y,learning_rate,iterations,parameters):
         dW = 1/size * np.dot(x.T, (sigma - y))
         db = 1/size * np.sum(sigma - y)
         weight -= learning_rate * dW
+        weight_history.append(weight)
         bias -= learning_rate * db 
+        bias_history.append(bias)
         if (i+1)%100==0:
             print('=== Iteration: %d ===' %(i+1))
             print('Training loss: %.4f' %loss)
@@ -336,3 +512,25 @@ for i in range(len(output_values)):
         count+=1
 accuracy=count/len(output_values)
 print("The Accuracy is", accuracy*100,"%")
+
+# ============================================================================
+# %% Get the final result for testing set
+# ============================================================================
+final_output=np.dot(test,parameters_out["weight"])+parameters_out["bias"]
+result=np.zeros(len(final_output))
+
+for i in range(len(final_output)):
+    if sigmoid(final_output[i])>=1/2:
+        result[i]=1
+    else:
+        result[i]=0
+        
+# ============================================================================
+# %% Save the result
+# ============================================================================
+result=np.reshape(result,(len(result),1))
+final_result=np.concatenate((passenger, result),axis=1)
+final_result=final_result.astype(int)
+dataframe=pd.DataFrame(final_result, columns=['PassengerId','Survived']) 
+print (dataframe)
+dataframe.to_csv('result.csv',index=False)
